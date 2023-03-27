f2ec33d51e40c2b783984b0939b1ccef73986b54
adv100500 <121932525+adv100500@users.noreply.github.com>
2023-03-27 20:13:02 +0200

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -2,10 +2,11 @@ pragma solidity >=0.6.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AngryOwl is ERC721URIStorage, ERC721Royalty, Ownable {
+contract AngryOwl is ERC721URIStorage, ERC721Enumerable, ERC721Royalty, Ownable {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -31,12 +32,22 @@ contract AngryOwl is ERC721URIStorage, ERC721Royalty, Ownable {
     }
 
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721,ERC721Royalty) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721,ERC721Royalty,ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
-    function _burn(uint256 tokenId) internal virtual override(ERC721URIStorage,ERC721Royalty) {
+    function _burn(uint256 tokenId) internal virtual override(ERC721,ERC721URIStorage,ERC721Royalty) {
         super._burn(tokenId);
         _resetTokenRoyalty(tokenId);
     }    
+
+        function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual override(ERC721,ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+
+    }
  
 }
